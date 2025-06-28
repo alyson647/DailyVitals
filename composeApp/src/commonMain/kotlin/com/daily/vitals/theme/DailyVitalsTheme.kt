ab86d6f80d70d0e4d953c870732a792f2422a026
alyson647 <alyson.roh@gmail.com>
2025-06-27 23:40:45 -0400

    Create theme

---
+++ composeApp/src/commonMain/kotlin/com/daily/vitals/theme/DailyVitalsTheme.kt
@@ -0,0 +1,35 @@
+package com.daily.vitals.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+val Color = lightColorScheme(
+    primary = Primary,
+    onPrimary = OnPrimary,
+    primaryContainer = PrimaryContainer,
+    onPrimaryContainer = OnPrimaryContainer,
+    inversePrimary = InversePrimary,
+    secondary = Secondary,
+    secondaryContainer = SecondaryContainer,
+    onSecondaryContainer = OnSecondaryContainer,
+    surface = Surface,
+    onSurface = OnSurface,
+    onSurfaceVariant = OnSurfaceVariant,
+    surfaceContainerLow = SurfaceContainerLow,
+    error = Error,
+    errorContainer = ErrorContainer,
+    outline = Outline,
+    outlineVariant = OutlineVariant
+)
+
+@Composable
+fun DailyVitalsTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = Color,
+        typography = Typography,
+        content = content
+    )
+}
