50adbe3cd253fad931207a10482869ff49417250
alyson647 <alyson.roh@gmail.com>
2025-06-29 19:43:09 -0400

    Add dependencies

--- composeApp/src/androidMain/kotlin/com/daily/vitals/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/daily/vitals/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.mmk.kmpauth.google.GoogleAuthCredentials
+import com.mmk.kmpauth.google.GoogleAuthProvider
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+//        GoogleAuthProvider.create(credentials = GoogleAuthCredentials(serverId = WebClientId))
+        GoogleAuthProvider.create(credentials = GoogleAuthCredentials(serverId = "1057388909480-8p4lrogr189j8cbruirophktregd05ch.apps.googleusercontent.com"))
 
         setContent {
             App()
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,16 +1,53 @@
 import SwiftUI
+import ComposeApp
+import GoogleSignIn
 import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(
+      _ app: UIApplication,
+      open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+      var handled: Bool
+
+      handled = GIDSignIn.sharedInstance.handle(url)
+      if handled {
+        return true
+      }
+
+      // Handle other custom URL types.
+
+      // If not handled by this app, return false.
+      return false
+    }
+
+
+}
 
 @main
 struct iOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
-    init() {
-        FirebaseApp.configure()
-    }
-    
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+   var body: some Scene {
+      WindowGroup {
+            ContentView().onOpenURL(perform: { url in
+                GIDSignIn.sharedInstance.handle(url)
+            })
+      }
+   }
 }
+
+//@main
+//struct iOSApp: App {
+//    
+//    init() {
+//        FirebaseApp.configure()
+//    }
+//    
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
