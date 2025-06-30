f801a450f8234766230637ea2779f9833ef5fd7c
alyson647 <alyson.roh@gmail.com>
2025-06-30 17:43:10 -0400

    Create rough header

---
+++ composeApp/src/commonMain/kotlin/com/daily/vitals/ui/home/Header.kt
@@ -0,0 +1,56 @@
+package com.daily.vitals.ui.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import dailyvitals.composeapp.generated.resources.Res
+import dailyvitals.composeapp.generated.resources.good_morning
+import dailyvitals.composeapp.generated.resources.other_health_data
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+fun HomeHeader(
+    name: String,
+) {
+    HomeHeaderImpl(
+        name = name
+    )
+}
+
+@Composable
+private fun HomeHeaderImpl(
+    name: String,
+) {
+    Row(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Column {
+            Text(
+                text = stringResource(Res.string.good_morning),
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                style = MaterialTheme.typography.titleSmall,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = name,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                style = MaterialTheme.typography.titleSmall,
+                textAlign = TextAlign.Center
+            )
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            text = "Profile photo",
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            style = MaterialTheme.typography.titleSmall,
+            textAlign = TextAlign.Center
+        )
+    }
+}
