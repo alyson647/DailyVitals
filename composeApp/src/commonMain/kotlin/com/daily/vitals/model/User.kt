982cfa65aaa785a9e13597ccec2c74fd5600552a
alyson647 <alyson.roh@gmail.com>
2025-08-04 20:12:35 -0400

    Remove profile and move fields to user data class

--- composeApp/src/commonMain/kotlin/com/daily/vitals/model/User.kt
+++ composeApp/src/commonMain/kotlin/com/daily/vitals/model/User.kt
@@ -4,5 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class User(
-    val profile: Profile,
+    val id: String,
+    val name: String,
+    val email: String,
+    val profilePicture: String
 )
