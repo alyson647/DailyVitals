020b8f69282b1b2c900b4d7c422f3575a2540da9
alyson647 <alyson.roh@gmail.com>
2025-08-08 22:53:13 -0400

    Remove unused functions in MainActivity

--- composeApp/src/androidMain/kotlin/com/daily/vitals/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/daily/vitals/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.daily.vitals.AppInitializer.onApplicationStart
-import com.google.firebase.Firebase
-import com.google.firebase.database.database
-import com.mmk.kmpauth.google.GoogleAuthCredentials
-import com.mmk.kmpauth.google.GoogleAuthProvider
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,23 +16,5 @@ class MainActivity : ComponentActivity() {
         setContent {
             App()
         }
-
     }
-}
-
-// this works but is android specific
-fun basicReadWrite() {
-    // [START write_message]
-    // Write a message to the database
-    val database = Firebase.database
-    val myRef = database.getReference("message")
-
-    myRef.setValue("Hello, World - iOS!")
-    // [END write_message]
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
