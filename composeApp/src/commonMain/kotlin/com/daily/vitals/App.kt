0b93f7359dad69cffa2f046fec41f7ace0cdb759
alyson647 <alyson.roh@gmail.com>
2025-08-08 22:53:30 -0400

    Use compose navigation

--- composeApp/src/commonMain/kotlin/com/daily/vitals/App.kt
+++ composeApp/src/commonMain/kotlin/com/daily/vitals/App.kt
@@ -1,80 +1,109 @@
 package com.daily.vitals
 
+import OnboardingDirections
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.daily.vitals.theme.DailyVitalsTheme
 import com.daily.vitals.ui.home.Home
 import com.daily.vitals.ui.onboarding.FirstOnboardingScreen
 import com.daily.vitals.ui.onboarding.SecondOnboardingScreen
 import com.daily.vitals.ui.onboarding.ThirdOnboardingScreen
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-enum class Screen {
-    FirstOnboarding,
-    SecondOnboarding,
-    ThirdOnboarding,
-    SignIn,
-    Home
-}
 
 @Composable
-@Preview
 fun App() {
     DailyVitalsTheme {
-        var currentScreen by remember { mutableStateOf(Screen.FirstOnboarding) }
+
+        val navController = rememberNavController()
+
+        NavHost(
+            navController = navController,
+            startDestination = Screen.FirstOnboarding.name
+        ) {
+            appGraph(
+                modifier = Modifier.fillMaxSize(),
+                navController = navController
+            )
+        }
+    }
+}
+
+internal fun NavGraphBuilder.appGraph(
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
+    composable(Screen.FirstOnboarding.name) {
+        FirstOnboardingScreen(
+            modifier = modifier
+        ) { directions ->
+            when (directions) {
+                is OnboardingDirections.Next -> navController.navigate(Screen.SecondOnboarding.name)
+                is OnboardingDirections.Home -> navController.navigate(Screen.Home.name)
+                else -> Unit
+            }
+        }
+    }
+    composable(Screen.SecondOnboarding.name) {
+        SecondOnboardingScreen(
+            modifier = modifier
+        ) { directions ->
+            when (directions) {
+                is OnboardingDirections.Next -> navController.navigate(Screen.ThirdOnboarding.name)
+                is OnboardingDirections.Home -> navController.navigate(Screen.Home.name)
+                is OnboardingDirections.Back -> navController.navigate(Screen.FirstOnboarding.name)
+            }
+        }
+    }
+    composable(Screen.ThirdOnboarding.name) {
         var showSignInDialog by remember { mutableStateOf(false) }
 
+        // TODO: move this information to viewmodel or somewhere else
         var signedInName by remember { mutableStateOf("") }
         var profileImage by remember { mutableStateOf("") }
 
-        Box(modifier = Modifier.fillMaxSize()) {
-            when (currentScreen) {
-                Screen.FirstOnboarding -> FirstOnboardingScreen(
-                    onSkipClick = { currentScreen = Screen.Home },
-                    onForwardClick = { currentScreen = Screen.SecondOnboarding }
-                )
-
-                Screen.SecondOnboarding -> SecondOnboardingScreen(
-                    onSkipClick = { currentScreen = Screen.Home },
-                    onForwardClick = { currentScreen = Screen.ThirdOnboarding },
-                    onBackClick = { currentScreen = Screen.FirstOnboarding }
-                )
-
-                Screen.ThirdOnboarding -> ThirdOnboardingScreen(
-                    onSkipClick = { showSignInDialog = true },
-                    onForwardClick = { showSignInDialog = true },
-                    onBackClick = { currentScreen = Screen.SecondOnboarding }
-                )
-
-                Screen.Home -> Home(signedInName, profileImage)
-
-                else -> {}
-            }
-
-            if (showSignInDialog) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colorScheme.onPrimaryContainer.copy(alpha = 0.1f))
-                )
-
-                GoogleSignInDialog(
-                    onSkipClick = { _, _ ->
-                        showSignInDialog = false
-                        currentScreen = Screen.Home
-                    },
-                    onButtonClick = { displayName, profileUrl ->
-                        signedInName = displayName
-                        profileImage = profileUrl
-                        showSignInDialog = false
-                        currentScreen = Screen.Home
-                    }
-                )
+        ThirdOnboardingScreen(
+            modifier = modifier
+        ) { directions ->
+            when (directions) {
+                is OnboardingDirections.Next -> showSignInDialog = true
+                is OnboardingDirections.Home -> navController.navigate(Screen.Home.name)
+                is OnboardingDirections.Back -> navController.navigate(Screen.SecondOnboarding.name)
             }
         }
+        if (showSignInDialog) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.onPrimaryContainer.copy(alpha = 0.1f))
+            )
+
+            // TODO: Add directions param for dialog
+            GoogleSignInDialog(
+                onSkipClick = { _, _ ->
+                    showSignInDialog = false
+                },
+                onButtonClick = { displayName, profileUrl ->
+                    // TODO: save this information somewhere
+                    signedInName = displayName
+                    profileImage = profileUrl
+                    showSignInDialog = false
+                    navController.navigate(Screen.Home.name) {
+                        popUpTo(Screen.ThirdOnboarding.name) { inclusive = true }
+                    }
+                }
+            )
+        }
+
+    }
+    composable(Screen.Home.name) {
+        Home("test name", "")
     }
 }
