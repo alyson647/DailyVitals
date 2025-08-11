a6f674587be217634250b8b06bdc55f744f842e7
alyson647 <alyson.roh@gmail.com>
2025-08-10 21:09:56 -0400

    Observe ui variable in home view model

--- composeApp/src/commonMain/kotlin/com/daily/vitals/feature/home/Home.kt
+++ composeApp/src/commonMain/kotlin/com/daily/vitals/feature/home/Home.kt
@@ -8,22 +8,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.daily.vitals.feature.home.component.HomeHeader
 import com.daily.vitals.feature.home.component.OtherHealthData
 import com.daily.vitals.feature.home.component.Summary
+import com.daily.vitals.ui.home.HomeViewModel
+import org.koin.core.annotation.KoinExperimentalAPI
+import androidx.compose.runtime.*
 
-
+@OptIn(KoinExperimentalAPI::class)
 @Composable
 internal fun Home(
     modifier: Modifier = Modifier,
-    directions: (AppDirections) -> Unit = {}
+    homeViewModel: HomeViewModel,
+    directions: (AppDirections) -> Unit = {},
 ) {
-    // TODO: get name, profile url information
+    val ui by homeViewModel.ui.collectAsState()
+
     // TODO: use directions for history screen once history screen created
     Column(
         modifier = modifier
@@ -32,12 +39,19 @@ internal fun Home(
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        HomeHeader(
-            name = "Buddy", // TODO: update with user name
-            profileUrl = "" // TODO: update with user profile image
-        )
-        Summary(modifier = Modifier.padding(horizontal = 16.dp))
-        Spacer(modifier = Modifier.height(16.dp))
-        OtherHealthData()
+        when {
+            ui.isLoading -> {
+                Spacer(Modifier.height(32.dp)); CircularProgressIndicator()
+            }
+            else -> {
+                HomeHeader(
+                    name = ui.user?.name ?: "",
+                    profileUrl = ui.user?.profilePicture ?: ""
+                )
+                Summary(modifier = Modifier.padding(horizontal = 16.dp))
+                Spacer(modifier = Modifier.height(16.dp))
+                OtherHealthData()
+            }
+        }
     }
 }
