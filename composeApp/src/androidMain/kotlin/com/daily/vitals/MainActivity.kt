d4670fc7f2b3a353a3bde13adec502046b4f54df
alyson647 <alyson.roh@gmail.com>
2025-08-12 21:42:48 -0400

    Clean up imports

--- composeApp/src/androidMain/kotlin/com/daily/vitals/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/daily/vitals/MainActivity.kt
@@ -4,12 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.ui.platform.LocalContext
 import com.daily.vitals.AppInitializer.onApplicationStart
-import com.daily.vitals.composeApp.database.DailyVitalsDatabase
-import com.daily.vitals.database.DatabaseDriverFactory
-import com.google.firebase.Firebase
-import com.google.firebase.database.database
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- composeApp/src/iosMain/kotlin/com/daily/vitals/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/daily/vitals/MainViewController.kt
@@ -1,8 +1,6 @@
 package com.daily.vitals
 
 import androidx.compose.ui.window.ComposeUIViewController
-import com.daily.vitals.composeApp.database.DailyVitalsDatabase
-import com.daily.vitals.database.DatabaseDriverFactory
 import com.daily.vitals.di.initKoin
 
 fun MainViewController() = ComposeUIViewController(
